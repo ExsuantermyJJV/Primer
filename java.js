18ed5f66addc096fb3151f2b00db5cc65898450b
ExsuantermyJJV <106329150+ExsuantermyJJV@users.noreply.github.com>
2022-10-06 16:00:32 -0500

    Update java.js

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('oct 11, 2022 16:38:00').getTime(),
+let countDown = new Date('oct 18, 2022 16:38:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
@@ -21,7 +21,7 @@ let countDown = new Date('oct 11, 2022 16:38:00').getTime(),
       hourr = minutee * 60,
       dayy = hourr * 24;
 
-let countDown1 = new Date('oct 11, 2022 16:38:00').getTime(),
+let countDown1 = new Date('oct 18, 2022 16:38:00').getTime(),
     x1 = setInterval(function() {
 
       let now = new Date().getTime(),
